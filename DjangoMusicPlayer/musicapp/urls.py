ae1c911b9d119b346d7ce12b712b1444244c8439
salmonGowa <gowasalmon@yahoo.com>
2024-01-15 07:16:37 +0300

    creating models

---
+++ DjangoMusicPlayer/musicapp/urls.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path,include
+from . import views
+
+
+app_name="APP"
+path("",views.index,name="index"),
